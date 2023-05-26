5f2adeeff769a234c4486400424e56d74a68200b
TheVikta <thevikta@gmail.com>
2023-05-26 20:52:17 +0300

    9. Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Program Entry
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3) == 0 && (i % 5) == 0)
+			printf("FizzBuzz");
+
+		else if ((i % 3) == 0)
+			printf("Fizz");
+
+		else if ((i % 5) == 0)
+			printf("Buzz");
+
+		else
+			printf("%d", i);
+
+		if (i == 100)
+			continue;
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
